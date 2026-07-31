4e1cb1b008c1e32829e64a73d680df9ac0541afb
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-07-31 15:17:35 +0000

    salvar tudo

---
+++ callback.js
@@ -0,0 +1,15 @@
+const nomes = ["Luan", "Ama", "Luana" ];
+
+//nomes.forEach(function  (nome){
+ //       console.log(nome);
+//});
+
+//nomes.forEach(function  (nome){
+//    console.log(nome);
+//})
+
+function imprimeNome(nome);{
+    console.log(nome);
+}
+
+nomes.forEach(imprimeNome);
