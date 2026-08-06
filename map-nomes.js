d18b27ef7387440e1d3a9109ef8ae073ec807d26
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-08-06 14:43:18 +0000

    salvvoooooooooooooooooo

---
+++ map-nomes.js
@@ -0,0 +1,7 @@
+const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva'];
+
+const nomesPadronizados = nomes.map((nomes) => {
+    return nomes.toUpperCase();
+});
+
+console.log(nomesPadronizados);
--- map.js
+++ map.js
@@ -1,5 +1,6 @@
-const notas [10, 9.5, 8, 7, 6];
+const notas = [10, 9.5, 8, 7, 6];
 
-notas.mao(()=>{
-    
-})
+const notasAtualizadas = notas.map((notas)=>{
+  return  notas + 1 >= 10 ? 10 : notas + 1;
+});
+console.log(notasAtualizadas);
