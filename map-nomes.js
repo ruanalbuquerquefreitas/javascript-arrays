adaa524369ecb495c2d3d756299153fdd52c684a
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-08-07 15:14:19 +0000

    saaaaaaaaaaaaallllllllllllllvoooooooo

--- map-nomes.js
+++ map-nomes.js
@@ -1,7 +1,5 @@
 const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva'];
 
-const nomesPadronizados = nomes.map((nomes) => {
-    return nomes.toUpperCase();
-});
+const nomesPadronizados = nomes.map((nomes) => nomes.toUpperCase());
 
 console.log(nomesPadronizados);
