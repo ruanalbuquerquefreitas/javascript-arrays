afda24c751e3c312bf804123f6df64f694bd9a58
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-08-14 14:48:58 +0000

    ljkjhkj

--- mediageral.js
+++ mediageral.js
@@ -3,10 +3,12 @@ const salaJava = [6, 5, 8, 9, 5, 6];
 const salaPython = [7, 3.5, 8, 9.5];
 
 function calculaMedia(notasDaSala){
-    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => {
-        return acumulador + nota;
-},0);
-console.log(somaDasNotas);
+    const somaDasNotas = notasDaSala.reduce ((acumulador, nota) => acumulador + nota, 0);
+    const media = somaDasNotas / notasDaSala.length;
+    return media;
+}
 
-};
-calculaMedia(salaJS);
+console.log(`a média da sala de JavaScript é ${calculaMedia(salaJS)}`);
+console.log(`a média da sala de Java é ${calculaMedia(salaJava)}`);
+console.log(`a média da sala de Python é ${calculaMedia(salaPython)}`);
+
